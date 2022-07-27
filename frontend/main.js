a7ac4674e1f1bba7eb4ba65eef27e7d5773168e2
dante di domenico <dante@didoda.com>
2022-07-27 16:06:56 +0200

    feat: eslint

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import './main.css'
-import "tailwindcss/tailwind.css"
+import 'tailwindcss/tailwind.css'
 
 const i18n = createI18n({
     legacy: false,
